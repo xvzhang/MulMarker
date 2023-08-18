f9a4994e98a0de4b64740778e3dc099f5b409ed4
Tina9 <xuhzhang@outlook.com>
2023-08-18 09:47:33 +0800

    change Port

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def get_chat_response():
     return render_template('upload.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
